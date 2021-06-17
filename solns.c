4ddea0dcef62fed70913c980719a8345c4765c57
Kajaruban14 <84712032+Kajaruban14@users.noreply.github.com>
2021-06-17 13:35:56 +0530

    Max

--- solns.c
+++ solns.c
@@ -1,2 +1,12 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
+int max(int arr[], int n){
+
+int max_num=-32768;
+
+ for(int i=0;i<n;i++){
+    if (max_num<arr[i])
+        max_num=arr[i];
+ }
+return max_num;
+}
